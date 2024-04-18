f0e2e0f9326995602034c8d1cb5cd36b510d9866
felipefferrada <felipe.ferrada@usm.cl>
2024-04-17 20:05:37 -0400

    se modifico el cliente para generar las solicitudes y esperar la respuesta desde el servidor

--- cliente/EquiposCliente.go
+++ cliente/EquiposCliente.go
@@ -4,25 +4,72 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
+	"sync"
+	"time"
 
 	pb "github.com/felipefferrada/Lab3-grpc/proto"
 	"google.golang.org/grpc"
 )
 
+const (
+	serverAddress = "localhost:50051"
+)
+
+func sendMessage(client pb.ChatServiceClient, wg *sync.WaitGroup, id int, message string, responseCh chan<- bool) {
+	defer wg.Done()
+
+	for {
+		msg := &pb.Message{Text: message}
+		resp, err := client.SendMessage(context.Background(), msg)
+		if err != nil {
+			log.Fatalf("Error al enviar mensaje: %v", err)
+		}
+
+		//el print con la respuesta y la solicitud
+		fmt.Printf("Hebra %d - Respuesta del servidor: %s\n", id, resp.GetText())
+
+		// Dependiendo de la respuesta, decide si enviar otro mensaje o terminar la hebra
+		if resp.GetText() == "true" || resp.GetText() == "1" {
+			responseCh <- true // Indica que la hebra debe terminar
+			return
+		}
+
+		time.Sleep(3 * time.Second) // Espera 3 segundos antes de enviar otro mensaje
+	}
+}
+
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Error al conectarse al servidor: %v", err)
 	}
 	defer conn.Close()
 
-	c := pb.NewChatServiceClient(conn)
+	client := pb.NewChatServiceClient(conn)
 
-	msg := &pb.Message{Text: "Hola desde el cliente"}
-	resp, err := c.SendMessage(context.Background(), msg)
-	if err != nil {
-		log.Fatalf("Error al enviar mensaje: %v", err)
+	var wg sync.WaitGroup
+	responseCh := make(chan bool)
+
+	// Inicia cuatro hebras concurrentes
+	//el mensaje enviado es tipo (Equipo, cantAT, cantMP)
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go sendMessage(client, &wg, i+1, fmt.Sprintf("%d, %d, %d", i+1, rand.Intn(30-20+1)+20, rand.Intn(15-10+1)+10), responseCh)
 	}
 
-	fmt.Println("Respuesta del servidor:", resp.GetText())
+	// Espera a que todas las hebras terminen
+	go func() {
+		wg.Wait()
+		close(responseCh)
+	}()
+
+	// Espera respuestas de las hebras para determinar si deben enviar más mensajes
+	for resp := range responseCh {
+		if resp {
+			fmt.Println("Hebra terminada.")
+		} else {
+			fmt.Println("Hebra enviando otro mensaje.")
+		}
+	}
 }
